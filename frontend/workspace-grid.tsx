903f49c6f57307f4708c63401fa31c0b9bfc9446
cindy <ciindy.wu@gmail.com>
2022-06-20 16:09:47 -1000

    ice cream styles

--- frontend/workspace-grid.tsx
+++ frontend/workspace-grid.tsx
@@ -4,17 +4,19 @@ import styles from './workspace-grid.module.css'
 export default function WorkspaceGrid() {
   return (
     <div className={styles.container}>
-      <div className={styles.nav}>
-        Nav
-      </div>
-      <div className={styles.sidebar}>
-        Sidebar
-      </div>
-      <div className={styles.main}>
-        Main
-      </div>
-      <div className={styles.gutter}>
-        Gutter
+      <div className={styles.grid}>
+        <div className={styles.nav}>
+          Nav
+        </div>
+        <div className={styles.sidebar}>
+          Sidebar
+        </div>
+        <div className={styles.main}>
+          Main
+        </div>
+        <div className={styles.gutter}>
+          Gutter
+        </div>
       </div>
     </div>
   )
